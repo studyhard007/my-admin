696582fdc61ee110e5ee50fc640290e1d5f34b27
kenny <yuanzhijiang@pinquest.cn>
2021-05-06 22:42:53 +0800

    chore: use flow, typescript, es6 into project

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // 通过 CommonJS 规范导入 CSS 模块
 require('./main.css');
 // 通过 CommonJS 规范导入 show 函数
-const show = require('./show.js');
+import  { show } from './show';
 // 执行 show 函数
 show('Webpack');
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   // JavaScript 执行入口文件
-  entry: './main.js',
+  entry: "./main",
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle.js',
+    filename: "bundle.js",
     // 把输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, "./dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, {loader: 'css-loader'}],
+        use: [MiniCssExtractPlugin.loader, { loader: "css-loader" }],
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader",
       },
     ],
+    
   },
   plugins: [new MiniCssExtractPlugin()],
+  devServer: {
+    open: false,
+  },
+  devtool: "source-map", // 输出 Source Map 方便在浏览器里调试 TypeScript 代码
 };
