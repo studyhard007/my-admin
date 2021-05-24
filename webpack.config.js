db49cbda0c761b1c4c110d6024cf17070959f293
kenny <yuanzhijiang@pinquest.cn>
2021-05-24 22:22:14 +0800

    chore: dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "管理输出",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
